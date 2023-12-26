0450c528594e4f0912409fc65b6b4cbca04ba068
shahdqalalweh <122091174+shahdqalalweh@users.noreply.github.com>
2023-12-26 12:32:55 +0200

    TicTac.dart

--- TicTac.dart
+++ TicTac.dart
@@ -1,109 +1,72 @@
 import 'dart:io';
 
-class TicTacToe {
-  List<String> board;
-  String currentPlayer;
-  int moves;
+bool winner = false, flag = true;
+int count = 0;
+//List for numbers from 1 to 9
+List<String> list = ['1', '2', '3', '4', '5', '6', '7', '8', '9'];
+//List for All possible possibilities to win
+List<String> comp = ['012', '048', '036', '147', '246', '258', '345', '678'];
 
-  TicTacToe() : board = List.filled(9, ' '), currentPlayer = 'X', moves = 0;
-  //To draw the external structure of the board
-  void printBoard() {
-    for (var i = 0; i < 9; i += 3) {
-      print(board.sublist(i, i + 3).join(' | '));
-      if (i < 6) print('---------');
-    }
+
+void main(List<String> args) {
+  printBoard();
+  user();
+}
+
+void  printBoard() {
+  //function to print shape of board
+   print('---+---+---');
+  print('| ${list[0]}  | ${list[1]} | ${list[2]} |');
+  print('---+---+---');
+  print('| ${list[3]}  | ${list[4]} | ${list[5]} |');
+  print('---+---+---');
+  print('| ${list[6]}  | ${list[7]} | ${list[8]} |');
+  print('---+---+---');
+}
+
+void user() {
+  //function for input users to play 
+  print('Choose Number For ${flag ? 'X' : 'O'}');
+  int num = int.parse(stdin.readLineSync()!);
+  list[num - 1] = flag ? 'X' : 'O';
+  count++;
+  flag = !flag;
+  if (count == 9) {
+    winner=true;
+    print('DRAW ...');
+  } else {
+    printBoard();
   }
-//To verify the number entered for the movement and move or stop
-  bool makeMove(int position) {
-    if (position < 1 || position > 9 || board[position - 1] != ' ') {
-      print('Invalid move. Try again.');
-      return false;
-    }
-
-    board[position - 1] = currentPlayer;
-    moves++;
-//To check the win in every move
-    if (checkWin() || checkDraw()) {
-      printBoard();
-      print('Game Over!');
-      return true;
-    }
-
-    currentPlayer = (currentPlayer == 'X') ? 'O' : 'X';
-    return false;
-  }
-//To check the win
-  bool checkWin() {
-    // Check rows, columns, and diagonals for a win
-    for (var i = 0; i < 3; i++) {
-      if (board[i] != ' ' &&
-          board[i] == board[i + 3] &&
-          board[i + 3] == board[i + 6]) {
-        printBoard();
-        print('Player $currentPlayer wins!');
-        return true;
-      }
-
-      if (board[i * 3] != ' ' &&
-          board[i * 3] == board[i * 3 + 1] &&
-          board[i * 3 + 1] == board[i * 3 + 2]) {
-        printBoard();
-        print('Player $currentPlayer wins!');
-        return true;
-      }
-    }
-
-    if (board[0] != ' ' && board[0] == board[4] && board[4] == board[8]) {
-      printBoard();
-      print('Player $currentPlayer wins!');
-      return true;
-    }
-
-    if (board[2] != ' ' && board[2] == board[4] && board[4] == board[6]) {
-      printBoard();
-      print('Player $currentPlayer wins!');
-      return true;
-    }
-
-    return false;
-  }
-
-  bool checkDraw() {
-    if (moves == 9) {
-      printBoard();
-      print('It\'s a draw!');
-      return true;
-    }
-    return false;
-  }
-
-  void restartGame() {
-    board = List.filled(9, ' ');
-    currentPlayer = 'X';
-    moves = 0;
+  check_winner('X');
+  check_winner('O');
+  if(winner==false){
+    user();
   }
 }
 
-void main() {
-  TicTacToe game = TicTacToe();
+bool check(String a, String b) {
+  List<int> n = a.split('').map((k) {
+    return int.parse(k);
+  }).toList();
+  for (final item in n) {
+    if (list[item] != b) {
+      return false;
+    }
+  }
+  return true;
+}
 
-  do {
-    print('\nCurrent Board:');
-    game.printBoard();
-    print('Player ${game.currentPlayer}, enter your move (1-9): ');
 
-    try {
-      int move = int.parse(stdin.readLineSync()!);
-      if (game.makeMove(move)) {
-        print('Do you want to play again? (yes/no)');
-        String playAgain = stdin.readLineSync()!.toLowerCase();
-        if (playAgain == 'yes') {
-          game.restartGame();
-        } else {
-          print('Thanks for playing!');
-          break;
-        }
-      }
+void check_winner(player) {
+  for (final item in comp) {
+    bool c = check(item, player);
+    if(c){
+      winner=true;
+      print('$player WON ....');
+      return;
+    }
+  }
+}
     } catch (e) {
       print('Invalid input. Please enter a number.');
     }
